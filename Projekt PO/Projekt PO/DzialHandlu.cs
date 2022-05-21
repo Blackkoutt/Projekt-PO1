dbd3855bb83c85140806d63dbde9c1b8d3eefbe3
Paweł Siemieniuk <89398353+pawelsiemieniuk@users.noreply.github.com>
2022-05-22 00:27:05 +0200

    Drobne zmiany
    *W ZlecenieDruku dodaje na liste publikacje jeśli jej nie ma
    *ZlecenieKupna uzupełnione
    *Nowa metoda UsunPublikacje

--- Projekt PO/Projekt PO/DzialHandlu.cs
+++ Projekt PO/Projekt PO/DzialHandlu.cs
@@ -15,7 +15,11 @@ namespace Wydawnictwo
         public static void ZlecenieDruku(int ilosc, Publikacje publikacje)
         {
             if (DzialProgramowy.WyborDrukarni(ilosc, publikacje))
+            {
                 Console.WriteLine("Pomyslnie wydrukowano " + publikacje.Tytul + " w nastepujacej liczbie egzemplarzy: " + ilosc);
+                if (!PublikacjaNaLiscie(publikacje))
+                    { DodajDoListy(publikacje); }
+            }
         }
         //dodaj do listy publikacji jesli drukarnia zwrocila true po wydrukowaniu
         public static void DodajDoListy(Publikacje publikacje)
@@ -24,7 +28,12 @@ namespace Wydawnictwo
         }
         //ew oddzielna klasa sklep
         public void ZlecenieKupna(int ilosc, Publikacje publikacje)
-        { }
+        {
+            if (publikacje.Ilosc - ilosc >= 0)
+                publikacje.setilosc(publikacje.Ilosc - ilosc);
+            else
+                Console.WriteLine("Nie ma wystarczającej ilości podanej publikacji, dostępna ilość to: " + publikacje.Ilosc);
+        }
         public List<Publikacje> Katalog
         {
             get { return ListaPublikacji; }
@@ -42,6 +51,11 @@ namespace Wydawnictwo
                 if (publikacja.Equals(publikacje)) { return true; }
             }
             return false;
+        }
+
+        public void UsunPublikacje(Publikacje publikacja)
+        { 
+            ListaPublikacji.Remove(publikacja); 
         }
 
     }
