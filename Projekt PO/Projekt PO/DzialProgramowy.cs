a2573dfbc0f51cdffd8a4689e20bee355be16ec3
Paweł Siemieniuk <89398353+pawelsiemieniuk@users.noreply.github.com>
2022-06-14 16:46:24 +0200

    Małe zmiany DP
    Metoda Zlecenie w krótszej formie
    Tam gdzie było potrzebne do działania Update w Programie dodałem odpowiednie obiekty jako parametry

--- Projekt PO/Projekt PO/DzialProgramowy.cs
+++ Projekt PO/Projekt PO/DzialProgramowy.cs
@@ -16,7 +16,7 @@ namespace Wydawnictwo
       // public DzialProgramowy() { DH = new DzialHandlu(); DR = new Drukarnie(); }
         //zamiana na boola żeby dostać komunikat czy umowa została zawarta (w obu umowach)
 
-        public void UmowaOPrace(double dlugosc, Autor autor)
+        public void UmowaOPrace(double dlugosc, Autor autor, DzialHandlu DH)
         {
             if (UmowaODzieloNaLiscie(autor) || UmowaOPraceNaLiscie(autor)) throw new AutorMaUmowe("Autor ma juz zawarta umowe o prace lub dzielo");
             if (!AutorNaLiscie(autor))
@@ -24,6 +24,7 @@ namespace Wydawnictwo
 
             UmowyOPrace umowa = new UmowyOPrace(dlugosc, autor);
             ListaUmow.Add(umowa);
+            Program.Update(DH, this);
         }
 
         //jesli autor ma juz umowe to nie dodajemy go do listy autorow i do listy umow
@@ -32,7 +33,7 @@ namespace Wydawnictwo
         //po wykonaniu zlecenia umowa powinna zostać rozwiązana czyli teoretycznie jeśli
         //potrzebujemy i tak publikacji do tego to można od razu ją usunąć i tworzenie jest bezsensowne
         //więc albo można ją usuwać ręcznie albo od razu po utworzeniu ewentualnie dodać listę z historią umów
-        public void UmowaODzielo(Autor autor, Publikacje publikacja)
+        public void UmowaODzielo(Autor autor, Publikacje publikacja, DzialHandlu DH)
         {
             if (UmowaODzieloNaLiscie(autor) || UmowaOPraceNaLiscie(autor)) throw new AutorMaUmowe("Autor ma juz zawarta umowe o prace lub dzielo");
             if (!AutorNaLiscie(autor))
@@ -40,103 +41,23 @@ namespace Wydawnictwo
 
             UmowyODzielo umowa = new UmowyODzielo(autor, publikacja);
             ListaUmow.Add(umowa);
+            Program.Update(DH, this);
         }
 
-        //usuniecie z listy umow obiektu ktory ma danego autora ew usuniecie tez tego obiektu
-        public void RozwiazanieUmowy(Autor autor)
+        //usuniecie z listy umow obiektu ktory ma danego autora
+        public void RozwiazanieUmowy(Autor autor, DzialHandlu DH)
         {
             for (int i = 0; i < ListaUmow.Count; i++)
             {
-                //jeśli za duży tasiemiec dodać var autorr = ListaUmow.Cast<UmowyOPrace>().ToList()[i].Autor; i drugie dla UmowyODzielo
                 if (ListaUmow[i] is UmowyOPrace && ListaUmow.Cast<UmowyOPrace>().ToList()[i].Autor != null && autor.Equals(ListaUmow.Cast<UmowyOPrace>().ToList()[i].Autor))
                 { ListaUmow.Remove(ListaUmow[i]); break; }
-                //BLAD
+
                 if (ListaUmow[i] is UmowyODzielo && ListaUmow.Cast<UmowyODzielo>().ToList()[i].Autor != null && autor.Equals(ListaUmow.Cast<UmowyODzielo>().ToList()[i].Autor))
                 { ListaUmow.Remove(ListaUmow[i]); break; }
             }
-
-            //po rozwiazaniu umowy napisane przez autora książki powinny być dalej sprzedawane więc autor też powinien zostać
-            /*foreach (Autor autorr in ListaAutorow)
-            {
-                if (autor.Equals(autorr)) { ListaAutorow.Remove(autorr); }
-            }*/
+            Program.Update(DH, this);
         }
 
-        // jesli autor ma umowe o prace to moze zrealizowac zlecenie
-        // jesli autor nie ma umowy o prace to nie moze zrealizowac zlecenia musi najpiew posiadac umowe
-        // jesli autor ma umowe o prace to wywoluje sie konsturktor ale danej publikacji
-        // trzeba najpierw sprawdzic typ danej publikacji
-        // dodanie do listy publikacji ale najpierw publikacja musi zostac wydrukowana
-
-        // to jest zlecenie napisania książki więc powinno tylko dostawać informacje
-        // potrzebne do stworzenia publikacji i dodać ją na liste
-
-        /*public String Zlecenie(int Wybor_Ksiazki, Autor autor,char Wybor_Rodzaju,string tytul,DzialHandlu DH)
-        {
-            if (Wybor_Rodzaju == '1' || Wybor_Rodzaju == '2')
-            {
-                switch (Wybor_Rodzaju)
-                {
-                    case '1':
-                        {
-                            switch (Wybor_Ksiazki)
-                            {
-                                case '1':
-                                    {
-                                        DH.DodajDoListy(new Sensacyjne(autor, tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                                case '2':
-                                    {
-                                        DH.DodajDoListy(new Kryminalistyczne(autor, tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                                case '3':
-                                    {
-                                        DH.DodajDoListy(new Fantasy(autor, tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                                case '4':
-                                    {
-                                        DH.DodajDoListy(new Romanse(autor, tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                                case '5':
-                                    {
-                                        DH.DodajDoListy(new Albumy(autor, tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                                case '6':
-                                    {
-                                        DH.DodajDoListy(new Inne(autor, tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                            }
-                            break;
-                        }
-                    case '2':
-                        {
-                            switch (Wybor_Ksiazki)
-                            {
-                                case '1':
-                                    {
-                                        DH.DodajDoListy(new Miesiecznik(tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-                                case '2':
-                                    {
-                                        DH.DodajDoListy(new Tygodnik(tytul));
-                                        return "Zlecenie zostalo poprawnie stworzone";
-                                    }
-
-                            }
-                            break;
-                        }
-                }
-                return "";
-            }
-            else return "Wystapil blad przy realizacji zlecenia";
-        }*/
         public bool Zlecenie(Autor? autor, String? rodzaj, String? tytul, DzialHandlu DH)
         {
             //Lista nazw klas nieporządanych przy tworzeniu obiektu publikacji
@@ -163,43 +84,45 @@ namespace Wydawnictwo
                 { Console.WriteLine("Nie udało się stworzyć zlecenia\n"); return false; }
 
                 DH.DodajDoListy(pub);
+                Program.Update(DH, this);
                 return true;
             }
             return false;
         }
 
 
-        public static Boolean WyborDrukarni(int ilosc, Publikacje publikacje, DzialHandlu DH)
+        public Boolean WyborDrukarni(int ilosc, Publikacje publikacje, DzialHandlu DH)
         {
             if (publikacje is Albumy)
-            { return Drukarnie.DrukujDobrze(ilosc, publikacje, DH); }
+            { return Drukarnie.DrukujDobrze(ilosc, publikacje, DH, this); }
             else
-            { return Drukarnie.DrukujNormalnie(ilosc, publikacje, DH); }
+            { return Drukarnie.DrukujNormalnie(ilosc, publikacje, DH, this); }
         }
 
-        public ArrayList getUmowy() 
+        public ArrayList getUmowy()
         {
             if (ListaUmow.Count != 0) { return ListaUmow; }
             else throw new PustaListaException("Lista umow jest pusta");
             
         }
 
-        public ArrayList getAutor() 
+        public ArrayList getAutor()
         {
             if(ListaAutorow.Count!=0) return ListaAutorow;
             throw new PustaListaException("Brak jakichkolwiek autorow na liscie autorow");
         }
 
-        //dodaj do pliku i dodaj na liste 
+         
         public void DodajAutora(Autor autor)
         {
             if (!AutorNaLiscie(autor)) { ListaAutorow.Add(autor); }
             else throw new AutorJestNaLiscie("Dany autor jest juz na liscie autorow");
         }
-        //tu lepiej zeby byla metoda ktora usunie wszystkie wystapienia
-        public void UsunAutora(Autor autor)
+        
+        public void UsunAutora(Autor autor, DzialHandlu DH)
         {
             ListaAutorow.Remove(autor);
+            Program.Update(DH, this);
         }
 
         public bool AutorNaLiscie(Autor autor)
